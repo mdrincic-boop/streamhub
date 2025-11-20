42f6d19a080d4bdc43cfa3e9b42dc90e08505b75
mdrincic-boop <mdrincic@gmail.com>
2025-11-20 08:36:56 +0100

    V2.1.7

--- server.cjs
+++ server.cjs
@@ -5,7 +5,7 @@ const { spawn } = require('child_process');
 require('dotenv').config();
 
 const supabaseUrl = process.env.VITE_SUPABASE_URL;
-const supabaseKey = process.env.VITE_SUPABASE_ANON_KEY;
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 const rtmpPort = parseInt(process.env.VITE_RTMP_PORT || '1935');
